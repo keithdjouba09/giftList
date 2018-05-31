b8a2450a9fa7de4de5b5d3dbd54406c5d4ae3c2a
keithdjouba09 <marlondjouba09@gmail.com>
2018-05-30 23:07:26 -0500

    Add project files.

---
+++ giftList/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(giftList.Startup))]
+namespace giftList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
