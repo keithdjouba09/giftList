a5160b652c2bf4a28c02c76dffaa1597229cf0f2
keithdjouba09 <marlondjouba09@gmail.com>
2018-06-14 11:47:59 -0500

    add views

---
+++ giftList/Controllers/GiftController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using giftList.Models;
+
+namespace giftList.Controllers
+{
+    public class GiftController : Controller
+    {
+        // GET: Gift/ Random
+        public ActionResult Random()
+        {
+            var gift = new Gift() { Name = "Disney Doll"};
+
+            var viewResult = new ViewResult();
+           
+            return View(gift);
+            //return Content("Hello World");
+        }
+       // [Route("gift/released/{year}/{month: regex(\\d{4}): range(1, 12)}")]
+
+       /* public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year+"/" + month);
+        }*/
+       
+    }
+}
