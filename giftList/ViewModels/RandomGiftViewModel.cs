29de726e6433986a245d32225b48451e5697bfa9
keithdjouba09 <marlondjouba09@gmail.com>
2018-06-14 12:14:40 -0500

    add properties to RandomGIftViewModel Class

--- giftList/ViewModels/RandomGiftViewModel.cs
+++ giftList/ViewModels/RandomGiftViewModel.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using giftList.Models;
 
 namespace giftList.ViewModels
 {
     public class RandomGiftViewModel
     {
+        public Gift Gift { get; set; }
+        public List<Wisher> Wishers { get; set; }
     }
 }
